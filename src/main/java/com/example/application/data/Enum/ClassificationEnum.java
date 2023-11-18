fdab41641c4d912a26c7d2be772b02f3869bcf93
Evgeny Saybert <sajberte@mail.ru>
2023-11-18 16:36:37 +0300

    INIT

--- src/main/java/com/example/application/data/Enum/ClassificationEnum.java
+++
@@ -1,4 +0,0 @@
-package com.example.application.data.Enum;
-
-public enum ClassificationEnum {
-}
--- src/main/java/com/example/application/views/MainView.java
+++ src/main/java/com/example/application/views/MainView.java
@@ -4,16 +4,28 @@ import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.dependency.Uses;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.messages.MessageInput;
+import com.vaadin.flow.component.messages.MessageList;
+import com.vaadin.flow.component.messages.MessageListItem;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @PageTitle("Classificator")
 @Route(value = "my-view")
 @RouteAlias(value = "")
 @Uses(Icon.class)
 public class MainView extends Composite<VerticalLayout> {
+
+    private MessageList messageList = new MessageList();
+
+    private MessageInput messageInput = new MessageInput();
 
     public MainView() {
 //        HorizontalLayout layoutRow = new HorizontalLayout();
@@ -43,9 +55,28 @@ public class MainView extends Composite<VerticalLayout> {
 //        getContent().add(layoutColumn2);
 //        layoutColumn2.add(messageList);
 //
-        MessageInput messageInput = new MessageInput();
+        messageInput.addSubmitListener(this::clickMessageInput);
+        getContent().add(messageList);
         getContent().add(messageInput);
     }
+
+    private void clickMessageInput(MessageInput.SubmitEvent e ){
+        String message = e.getValue();
+        if(message == null || message.isBlank()){
+            return;
+        }
+        String classification = "Какая-то классификация";//TODO::вызвать сервис класификации
+        String priority = "Какая-то приоритезация";//TODO::вызвать сервис приоритезации
+        String employee = "Какая-то сотрудник";//TODO::вызвать сервис подсчета по сотрудникам
+
+        MessageListItem mItemClassification = new MessageListItem(classification, LocalDateTime.now().minusDays(1).toInstant(ZoneOffset.UTC), "Classificator");
+        MessageListItem mItemPriority = new MessageListItem(priority, LocalDateTime.now().minusDays(1).toInstant(ZoneOffset.UTC), "Prioritesator");
+        MessageListItem mItemEmployee = new MessageListItem(employee, LocalDateTime.now().minusDays(1).toInstant(ZoneOffset.UTC), "Employee");
+
+
+        messageList.setItems(mItemClassification,mItemPriority,mItemEmployee);
+    }
+
 
 //    private void setAvatarItemSampleData(AvatarItem avatarItem) {
 //        avatarItem.setHeading("Aria Bailey");
